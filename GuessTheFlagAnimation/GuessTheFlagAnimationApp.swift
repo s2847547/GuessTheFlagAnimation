abb4079d97ab508cc49681c237228777d6fb4c5a
s2847547 <s2847547@students.fscj.edu>
2025-10-02 17:15:34 -0400

    Initial Commit

---
+++ GuessTheFlagAnimation/GuessTheFlagAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagAnimationApp.swift
+//  GuessTheFlagAnimation
+//
+//  Created by Corey Burgos on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
